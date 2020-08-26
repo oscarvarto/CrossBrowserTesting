d403fa5f7301fae8fd2150969751bca8d19b1188
Oscar Vargas Torres <oscarvarto@protonmail.com>
2020-08-25 22:08:18 -0500

    Adding logging with SiftingAppender

--- build.sbt
+++ build.sbt
@@ -5,6 +5,7 @@ version := "0.1"
 scalaVersion := "2.13.3"
 
 libraryDependencies ++= Seq(
+  "ch.qos.logback" % "logback-classic" % "1.2.3",
   "io.github.bonigarcia" % "webdrivermanager" % "4.1.0" % Test,
   "org.scalactic" %% "scalactic" % "3.2.2" % Test,
   "org.scalatest" %% "scalatest" % "3.2.2" % Test,
--- src/test/scala/io/oscarvarto/ParallelTitleSuite.scala
+++ src/test/scala/io/oscarvarto/ParallelTitleSuite.scala
@@ -1,21 +1,26 @@
 package io.oscarvarto
 
-import org.scalatest.Suite
+import org.scalatest.{ParallelTestExecution, Suite}
 import org.scalatest.funsuite.{AnyFunSuite, AnyFunSuiteLike}
 import org.scalatest.matchers.should
 import org.scalatestplus.selenium.{Driver, WebBrowser}
+import org.slf4j.{LoggerFactory, MDC}
 
-abstract class TitleSuite(val url: String, expectedTitle: String) extends AnyFunSuiteLike with should.Matchers {
+class TitleSuite(val url: String, expectedTitle: String) extends AnyFunSuiteLike with should.Matchers {
   this: WebBrowser with Driver =>
 
+  val log = LoggerFactory.getLogger(s"$expectedTitle")
+
   test(s"$url should have title $expectedTitle") {
+    MDC.put("logFileName", expectedTitle)
     go to url
+    log.debug(s"Page title: $pageTitle")
     pageTitle shouldBe expectedTitle
     quit()
   }
 }
 
-class ParallelTitleSuite extends AnyFunSuite {
+class ParallelTitleSuite extends AnyFunSuite with ParallelTestExecution {
   import org.scalatest.prop.Tables._
 
   val titleData = Table(
